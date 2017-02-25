4416150ecae6e43aeb2c56225db3ed9c94203e87
Drew Graham <drewgraham@Drews-MacBook-Pro.local>
2017-02-25 18:28:04 -0500

    Pushed stupid build of game (mac version only right now)

---
+++ Milo Yiannopoopyloop/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour 
+{
+    
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionStay2D(Collision2D col) 
+	{
+        if (col.gameObject.name == "Poop")
+		{
+            col.gameObject.GetComponent<Rigidbody2D>().velocity.Set(-4, 0);
+            Debug.Log("collided");
+		}
+
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        
+    }
+
+}
